b1eaf88898777313ad4259ebd0eaa58f08fada10
Wagner Landgraf <info@landgraf.dev>
2022-07-25 19:59:14 -0300

    Added distribution units

---
+++ Dist/OpenApiRest.pas
@@ -0,0 +1,239 @@
+unit OpenApiRest;
+
+interface
+
+uses
+  SysUtils, Classes,
+  OpenApiJson;
+
+type
+  IRestResponse = interface
+  ['{C2CE5CD8-FA9F-442F-9980-988A2A0EFF3D}']
+    function StatusCode: Integer;
+    function ContentAsString: string;
+    function ContentAsBytes: TBytes;
+  end;
+
+  IRestRequest = interface
+  ['{55328D2F-FC30-48C7-9578-5A8A9152E4DA}']
+    procedure SetUrl(const Url: string);
+    procedure SetMethod(const Method: string);
+    procedure AddQueryParam(const Name, Value: string);
+    procedure AddUrlParam(const Name, Value: string);
+    procedure AddBody(const Value: string);
+    function Execute: IRestResponse;
+  end;
+
+  IRestRequestFactory = interface
+  ['{3F581342-8522-44BD-8D42-1CAFE7DD7CC1}']
+    function CreateRequest: IRestRequest;
+  end;
+
+  TRestRequest = class(TInterfacedObject, IRestRequest)
+  private
+    FUrl: string;
+    FMethod: string;
+    FQueryParams: TStrings;
+    FUrlParams: TStrings;
+    FBody: string;
+  protected
+    function BuildUrl: string;
+    function PercentEncode(const Value: string): string; virtual;
+    property Body: string read FBody;
+    property Method: string read FMethod;
+  public
+    constructor Create;
+    destructor Destroy; override;
+    procedure SetUrl(const Url: string);
+    procedure SetMethod(const Method: string);
+    procedure AddQueryParam(const Name, Value: string); virtual;
+    procedure AddUrlParam(const Name, Value: string); virtual;
+    procedure AddBody(const Value: string); virtual;
+    function Execute: IRestResponse; virtual; abstract;
+  end;
+
+  EOpenApiClientException = class(Exception)
+  private
+    FResponse: IRestResponse;
+  public
+    constructor Create(const Msg: string; Response: IRestResponse);
+    property Response: IRestResponse read FResponse;
+  end;
+
+  TCustomRestService = class(TInterfacedObject)
+  private
+    FConverter: TCustomJsonConverter;
+    FRequestFactory: IRestRequestFactory;
+    FBaseUrl: string;
+    procedure SetBaseUrl(const Value: string);
+  protected
+    procedure CheckError(Response: IRestResponse);
+    function CreateConverter: TCustomJsonConverter;
+    function Converter: TCustomJsonConverter;
+    property RequestFactory: IRestRequestFactory read FRequestFactory;
+  public
+    constructor Create(const BaseUrl: string);
+    destructor Destroy; override;
+    function CreateRequest(const UrlPath, HttpMethod: string): IRestRequest;
+    property BaseUrl: string read FBaseUrl write SetBaseUrl;
+  end;
+
+var
+  DefaultRequestFactory: IRestRequestFactory;
+
+implementation
+
+uses
+  // refactor this later
+  OpenApiHttp;
+
+{ TRestService }
+
+procedure TCustomRestService.CheckError(Response: IRestResponse);
+begin
+  if (Response.StatusCode < 200) or (Response.StatusCode >= 300) then
+    raise EOpenApiClientException.Create('Request failed', Response);
+end;
+
+function TCustomRestService.Converter: TCustomJsonConverter;
+begin
+  if FConverter = nil then
+    FConverter := CreateConverter;
+  Result := FConverter;
+end;
+
+constructor TCustomRestService.Create(const BaseUrl: string);
+begin
+  inherited Create;
+  Self.BaseUrl := BaseUrl;
+  FRequestFactory := DefaultRequestFactory;
+end;
+
+function TCustomRestService.CreateConverter: TCustomJsonConverter;
+begin
+  Result := TCustomJsonConverter.Create;
+end;
+
+function TCustomRestService.CreateRequest(const UrlPath, HttpMethod: string): IRestRequest;
+var
+  Url: string;
+begin
+  Result := RequestFactory.CreateRequest;
+  Url := BaseUrl;
+  if (Length(UrlPath) > 0) and (UrlPath[1] <> '/') then
+    Url := Url + '/';
+  Url := Url + UrlPath;
+  Result.SetUrl(Url);
+  Result.SetMethod(HttpMethod);
+end;
+
+destructor TCustomRestService.Destroy;
+begin
+  FConverter.Free;
+  inherited;
+end;
+
+procedure TCustomRestService.SetBaseUrl(const Value: string);
+begin
+  if Value = '' then
+    raise EArgumentException.Create('Invalid BaseUrl');
+  FBaseUrl := Value;
+
+  // Normalize BaseUrl by removing trailing slash
+  if (Length(FBaseUrl) > 0) and (FBaseUrl[Length(FBaseUrl)] = '/') then
+    FBaseUrl := Copy(FBaseUrl, 1, Length(FBaseUrl) - 1);
+end;
+
+{ TRestRequest }
+
+procedure TRestRequest.AddBody(const Value: string);
+begin
+  FBody := Value;
+end;
+
+procedure TRestRequest.AddQueryParam(const Name, Value: string);
+begin
+  FQueryParams.Values[Name] := Value;
+end;
+
+procedure TRestRequest.AddUrlParam(const Name, Value: string);
+begin
+  FUrlParams.Values[Name] := Value;
+end;
+
+function TRestRequest.BuildUrl: string;
+var
+  I: Integer;
+  Name, Value: string;
+  Query: string;
+begin
+  Result := FUrl;
+  for I := 0 to FUrlParams.Count - 1 do
+  begin
+    Name := FUrlParams.Names[I];
+    Value := PercentEncode(FUrlParams.ValueFromIndex[I]);
+
+    Result := StringReplace(Result, '{' + Name + '}', Value, [rfIgnoreCase, rfReplaceAll]);
+  end;
+
+  Query := '';
+  for I := 0 to FQueryParams.Count - 1 do
+  begin
+    Name := FQueryParams.Names[I];
+    Value := PercentEncode(FQueryParams.ValueFromIndex[I]);
+    if Query <> '' then
+      Query := Query + '&';
+    Query := Query + Name + '=' + Value;
+  end;
+
+  if Query <> '' then
+    Result := Result + '?' + Query;
+end;
+
+constructor TRestRequest.Create;
+begin
+  inherited Create;
+  FQueryParams := TStringList.Create;
+  FUrlParams := TStringList.Create;
+end;
+
+destructor TRestRequest.Destroy;
+begin
+  FUrlParams.Free;
+  FQueryParams.Free;
+  inherited;
+end;
+
+function TRestRequest.PercentEncode(const Value: string): string;
+begin
+  {$MESSAGE WARN 'Implement'}
+  Result := Value;
+end;
+
+procedure TRestRequest.SetMethod(const Method: string);
+begin
+  FMethod := Method;
+end;
+
+procedure TRestRequest.SetUrl(const Url: string);
+begin
+  FUrl := Url;
+end;
+
+{ EOpenApiClientException }
+
+constructor EOpenApiClientException.Create(const Msg: string; Response: IRestResponse);
+var
+  Content: string;
+  ErrorMsg: string;
+begin
+  ErrorMsg := Msg + sLineBreak + 'status: ' + IntToStr(Response.StatusCode);
+  Content := Response.ContentAsString;
+  if Content <> '' then
+    ErrorMsg := ErrorMsg + sLineBreak + 'Response: ' + Copy(Content, 1, 512);
+  inherited Create(ErrorMsg);
+end;
+
+initialization
+  DefaultRequestFactory := THttpRestRequestFactory.Create;;
+end.
---
+++ Dist/OpenApiSparkle.pas
@@ -0,0 +1,98 @@
+unit OpenApiSparkle;
+
+interface
+
+uses
+  SysUtils, OpenApiRest,
+  Sparkle.Http.Client;
+
+type
+  TSparkleRestRequest = class(TRestRequest)
+  public
+    function Execute: IRestResponse; override;
+  end;
+
+  TSparkleRestResponse = class(TInterfacedObject, IRestResponse)
+  strict private
+    FClient: THttpClient;
+    FResponse: IHttpResponse;
+  public
+    constructor Create(Response: IHttpResponse; Client: THttpClient);
+    destructor Destroy; override;
+    function StatusCode: Integer;
+    function ContentAsString: string;
+    function ContentAsBytes: TBytes;
+  end;
+
+  TSparkleRestRequestFactory = class(TInterfacedObject, IRestRequestFactory)
+  public
+    function CreateRequest: IRestRequest;
+  end;
+
+implementation
+
+{ TSparkleRestRequestFactory }
+
+function TSparkleRestRequestFactory.CreateRequest: IRestRequest;
+begin
+  Result := TSparkleRestRequest.Create;
+end;
+
+{ TSparkleRestRequest }
+
+function TSparkleRestRequest.Execute: IRestResponse;
+var
+  Request: THttpRequest;
+  Response: IHttpResponse;
+  Client: THttpClient;
+begin
+  Client := THttpClient.Create;
+  try
+    Request := Client.CreateRequest;
+    try
+      Request.Uri := BuildUrl;
+      Request.Method := Self.Method;
+      if Body <> '' then
+        Request.SetContent(TEncoding.UTF8.GetBytes(Body));
+      Response := Client.Send(Request);
+      Result := TSparkleRestResponse.Create(Response, Client);
+      Client := nil;
+    finally
+      Request.Free;
+    end;
+  finally
+    Client.Free;
+  end;
+end;
+
+{ TSparkleRestResponse }
+
+constructor TSparkleRestResponse.Create(Response: IHttpResponse; Client: THttpClient);
+begin
+  inherited Create;
+  FResponse := Response;
+  FClient := Client;
+end;
+
+destructor TSparkleRestResponse.Destroy;
+begin
+  FClient.Free;
+  inherited;
+end;
+
+function TSparkleRestResponse.StatusCode: Integer;
+begin
+  Result := FResponse.StatusCode;
+end;
+
+function TSparkleRestResponse.ContentAsBytes: TBytes;
+begin
+  Result := FResponse.ContentAsBytes;
+end;
+
+function TSparkleRestResponse.ContentAsString: string;
+begin
+  Result := TEncoding.UTF8.GetString(FResponse.ContentAsBytes);
+end;
+
+end.
